a9f8199c8eaaddb05aa365174c81a20728d88af8
Robin Baby <robin.baby@qburst.com>
2026-04-15 23:21:07 +0530

    Add api-gateway

---
+++ gateway/src/main/java/com/example/kaf/gateway/GatewayApplication.java
@@ -0,0 +1,30 @@
+package com.example.kaf.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("order-service", r -> r
+                        .path("/api/orders/**")   // 🔥 FIX
+                        .uri("lb://ORDER"))
+
+                .route("notify-service", r -> r
+                        .path("/api/logs/**")
+                        .uri("lb://NOTIFY"))
+
+                .build();
+    }
+}
